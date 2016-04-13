1d6cf2a3c1d76153a157f84b76f4d88616f0dc36
totalamd <totalamd@users.noreply.github.com>
2016-04-13 16:33:08 +0300

    add x.mipt.cc support to 'user search' function

--- zlowiki.user.js
+++ zlowiki.user.js
@@ -93,25 +93,19 @@ const l = function(){}, i = function(){};
 
 //user search link next to his nick
 (function() {
+	let siteSearch;
 	let user_nick, user_host;
 	
-	if (!document.querySelector('.nn') && !document.querySelector('.unreg')) return false;
-	
-	user_nick = document.querySelector('.nn') || document.querySelector('.unreg');		
-	user_nick.value = encodeURIComponent(user_nick.innerText);
-	user_host = document.querySelector('.nn + small') || document.querySelector('.unreg + small');
-	user_host.value = encodeURIComponent(user_host.innerText.slice(1, -1));
-
-	const create_link = (url, text, title) => {
+	const createLink = (url, text, title, invisible) => {
 		const a = document.createElement('a');
 		a.href = url;
 		a.text = text;
 		a.title = title;
 		a.target = '_blank';
-		a.style.color = 'green';
-		a.style.fontWeight = 'bold';
-		a.style.fontSize = 'small';
-		a.style.marginLeft = '0.2em';
+		a.className = 'user_search';
+		if (invisible) {
+			a.className += ' user_search_invisible';
+		}
 		return a;
 	};
 
@@ -119,8 +113,6 @@ const l = function(){}, i = function(){};
 		where.parentNode.insertBefore(what, where.nextSibling);
 	};
 
-	let siteSearch;
-	
 	switch (location.hostname) {
 		case 'x.mipt.cc':
 			siteSearch = 12;
@@ -133,9 +125,62 @@ const l = function(){}, i = function(){};
 			siteSearch = 0;
 	}
 
-	insertAfter(create_link(`http://zlo.rt.mipt.ru:7500/nickhost.jsp?site=${siteSearch}&w=n&t=${user_nick.value}`, 'h', 'Хосты этого ника'), user_nick);
-	insertAfter(create_link(`http://zlo.rt.mipt.ru:7500/search?site=${siteSearch}&nick=${user_nick.value}`, '?', 'Сообщения этого пользователя'), user_nick);
-	insertAfter(create_link(`http://zlo.rt.mipt.ru:7500/search?site=${siteSearch}&nick=${user_nick.value}&host=${user_host.value}`, '?nh', 'Сообщения этого ника с этого хоста'), user_host);
-	insertAfter(create_link(`http://zlo.rt.mipt.ru:7500/nickhost.jsp?site=${siteSearch}&w=h&t=${user_host.value}`, 'n', 'Ники этого хоста'), user_host);
-	insertAfter(create_link(`http://zlo.rt.mipt.ru:7500/search?site=${siteSearch}&host=${user_host.value}`, '?', 'Сообщения с этого хоста'), user_host);
+	GM_addStyle(`
+		a.user_search {
+			color: green;
+			font-weight: bold;
+			font-size: small !important;
+			margin-left: 0.2em;
+		}
+		a.user_search_invisible {
+			display: none;
+		}
+		span[id^="m"]:hover a.user_search_invisible {
+			display: inline;
+		}
+	`);
+
+	// current message in particular thread handling
+	if (document.querySelector('.nn + small') || document.querySelector('.unreg + small')) {
+		user_nick = document.querySelector('.nn') || document.querySelector('.unreg');		
+		user_nick.value = encodeURIComponent(user_nick.innerText);
+		user_host = document.querySelector('.nn + small') || document.querySelector('.unreg + small');
+		user_host.value = encodeURIComponent(user_host.innerText.slice(1, -1));
+
+		insertAfter(createLink(`http://zlo.rt.mipt.ru:7500/nickhost.jsp?site=${siteSearch}&w=n&t=${user_nick.value}`, 'h', 'Хосты этого ника'), user_nick);
+		insertAfter(createLink(`http://zlo.rt.mipt.ru:7500/search?site=${siteSearch}&nick=${user_nick.value}`, '?', 'Сообщения этого пользователя'), user_nick);
+		insertAfter(createLink(`http://zlo.rt.mipt.ru:7500/search?site=${siteSearch}&nick=${user_nick.value}&host=${user_host.value}`, '?nh', 'Сообщения этого ника с этого хоста'), user_host);
+		insertAfter(createLink(`http://zlo.rt.mipt.ru:7500/nickhost.jsp?site=${siteSearch}&w=h&t=${user_host.value}`, 'n', 'Ники этого хоста'), user_host);
+		insertAfter(createLink(`http://zlo.rt.mipt.ru:7500/search?site=${siteSearch}&host=${user_host.value}`, '?', 'Сообщения с этого хоста'), user_host);
+	}
+
+	// other messages handling, in individual thread and in global index
+	const postsList = Array.from(document.querySelectorAll('span[id^="m"]'));
+	postsList.forEach((post) => {
+		let where_to_post_links;
+
+		switch (location.hostname) {
+		case 'zlo.rt.mipt.ru':
+		case 'board.rt.mipt.ru':
+			user_nick = post.lastElementChild;
+			user_nick.value = encodeURIComponent(user_nick.innerText);
+			user_host = post.lastChild;
+			user_host.value = encodeURIComponent(user_host.textContent.match(/.*\((.*)\).*/)[1]);
+			where_to_post_links = user_host;
+			break;
+		case 'x.mipt.cc':
+			user_nick = post.querySelector('.unr, .reg');
+			user_nick.value = encodeURIComponent(user_nick.innerText);
+			user_host = post.querySelector('span.nobr:nth-last-of-type(3)');
+			user_host.value = encodeURIComponent(user_host.textContent.match(/.*\((.*)\).*/)[1]);
+			where_to_post_links = post.lastChild;
+			break;
+		}
+
+		insertAfter(createLink(`http://zlo.rt.mipt.ru:7500/search?site=${siteSearch}&nick=${user_nick.value}&host=${user_host.value}`, '?nh', 'Сообщения этого ника с этого хоста', true), where_to_post_links);
+		insertAfter(createLink(`http://zlo.rt.mipt.ru:7500/nickhost.jsp?site=${siteSearch}&w=h&t=${user_host.value}`, 'n', 'Ники этого хоста', true), where_to_post_links);
+		insertAfter(createLink(`http://zlo.rt.mipt.ru:7500/search?site=${siteSearch}&host=${user_host.value}`, '| ?', 'Сообщения с этого хоста', true), where_to_post_links);
+		insertAfter(createLink(`http://zlo.rt.mipt.ru:7500/nickhost.jsp?site=${siteSearch}&w=n&t=${user_nick.value}`, 'h', 'Хосты этого ника', true), where_to_post_links);
+		insertAfter(createLink(`http://zlo.rt.mipt.ru:7500/search?site=${siteSearch}&nick=${user_nick.value}`, '?', 'Сообщения этого пользователя', true), where_to_post_links);
+		});
 })();
